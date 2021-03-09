6b755fa127c88db14bed4dc13d2c42b6e8263170
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-09 16:56:07 +0200

    klaseje

--- Bebras.php
+++ Bebras.php
@@ -2,6 +2,73 @@
 class Bebras {
     public $tail = 'BIG';
     private $age = 23;
+    public $property = "blalalal";
+
+    public static $legs = 2;
+
+    private $wife = "blond", $children1 = "beaver1", $children2 = "beaver3", $dog = "pufik";
+    
+    private $BebroObjektas;
+    static public function createBebras($age)
+    {
+        echo 'Lets create';
+        return self::$BebroObjektas ?? self::$BebroObjektas = new self($age);
+    }
+
+    public function __construct($age)
+    {
+        echo '<br>';
+        echo 'Konstruojam: ';
+
+        echo self::$legs;
+        self::$legs = 85;
+
+        echo '<br>';
+        if(is_integer($age)) {
+            $this->init($age);
+        }
+        if(is_string($age)) {
+            $this->init2($age);
+        }
+        // $this->age = $age;
+    }
+
+    public function init($age)
+    {
+        echo '<br>';
+        echo 'INIT';
+        echo '<br>';
+        $this->age = $age;
+    }
+
+    public function init2($age)
+    {
+        echo '<br>';
+        echo 'INIT2';
+        echo '<br>';
+        $this->wife = $age;
+    }
+
+    public function __get($property)
+    {
+        if(!in_array($property, ['wife', 'children1'])) {
+            return;
+        }
+        // echo "Stebuklu metas $property";
+        return $this->$property;
+    }
+
+    public function __set($property, $value)
+    {
+
+        if("$age" == $property && $value > 30) {
+            echo 'blogi metai';
+            return;
+        }
+
+        $this->$property = $value;
+
+    }
 
     public function sayTail()
     {
@@ -15,6 +82,15 @@ class Bebras {
         echo '<br>';
         echo "Bebras age: ".$this->age;
         echo '<br>';
+    }
+
+    public function setAge($age)
+    {
+        if($age > 30) {
+            echo 'blogi metai';
+            return;
+        }
+        $this->age = $age;   
     }
 }
 
--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 include __DIR__.'/Bebras.php';
 include __DIR__.'/Udra.php';
 
-$objektas= new Bebras;
+// echo Bebras::$legs = 55;
+Bebras::createBebras();
+$objektas= new Bebras(12);
 $obj2 = new Bebras;
 $obj3= new Udra;
 
 $objektas->tail = "Small";
-echo $objektas->tail;
+echo Bebras::$legs;
+// echo $objektas->tail;
 
-$objektas->sayTail();
-$objektas->getAge();
+// $objektas->sayTail();
+// $objektas->setAge(40);
+// $objektas->getAge();
+
+
+$objektas->$Udra;
+echo $objektas->dog; //privatus metodas
+
+
+
+echo '<pre>';
+var_dump($objektas);
 ?>
