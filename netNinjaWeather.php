932b29ba7b2025af0344448dc4deee02ea60f568
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-03-16 08:51:50 +0200

    sayHello static

--- netNinjaWeather.php
+++ netNinjaWeather.php
@@ -16,4 +16,26 @@ class Weather {
 
 echo Weather::determineTempConditions(50);
 
+
+
+class Greeting {
+
+    public static function sayHello($language) {
+        if($language === 'en') {
+            echo "<h1>Hello World</h1>";
+        } elseif ($language === 'lt') {
+            echo "<h1 style='color:pink';>Labas rytas</h1>";
+        } elseif ($language === 'pl') {
+            echo "<h1 style='color:blue';>Dzień Dobry</h1>";
+        } elseif ($language === 'es') {
+            echo "<h1 style='color:yellow';>Buenos dias</h1>";
+        } elseif($language === 'rus') {
+            echo "<h1 style='color:green';>Здравствуйте</h1>";
+        } else {
+            echo "<h1 style='color:red';>IDK what language is, sorry</h1>";
+        }
+    }
+}
+
+Greeting::sayHello('lt');
 ?>
